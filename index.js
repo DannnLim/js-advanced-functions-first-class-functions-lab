cb1e78a478d88fe09fa68bc83cfb6f738bc0744f
DannnLim <Dannylim-@hotmail.com>
2022-06-11 20:52:51 +1000

    functions

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+function returnFirstTwoDrivers() {
+    return ["Sally", "Bob"]
+}
+
+function returnLastTwoDrivers() {
+    return ["Freddy", "Claudia"]
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function Driver() {
+    if (Driver === 0) {
+        return selectingDrivers;
+    }
+}
+
+const fareMultiplier = createFareMultiplier(2);
+function createFareMultiplier() {
+    return function (fareMultiplier) {
+        return (fareMultiplier * fareMultiplier)
+    }
+}
+const Doubler = fareDoubler;
+function fareDoubler(Doubler) {
+    return (Doubler * 2);
+}
+
+function fareTripler(Triple) {
+    return (Triple * 3);
+}
+
+function selectDifferentDrivers(arrayOfDrivers, funct) {
+    return funct(arrayOfDrivers);
+}
